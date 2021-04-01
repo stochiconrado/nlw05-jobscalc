6fd194bef8085cee8a8a5db6758562ea1be7e134
Conrado Stochi <stochiconrado@gmail.com>
2021-04-01 14:33:33 -0300

    Aula 2

--- src/server.js
+++ src/server.js
@@ -8,7 +8,10 @@ server.set('view engine','ejs')
 //habilitar statics archive
 server.use(express.static("public"))
 
+//habilitar req.body
+server.use(express.urlencoded({extended:true}))
+
 //routes
 server.use(routes)
 
-server.listen(3333, () => console.log("RUNNING"))
+server.listen(3000, () => console.log("RUNNING"))
